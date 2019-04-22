ea83e423a3e04241b79421a2cffb8e5846227a88
Cattusorb <magicmachineplaymc@gmail.com>
2019-04-22 09:52:50 -0400

    new splash screen

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -4,20 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton play;
+    private Button play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
 
-        play = findViewById(R.id.play);
+        play = findViewById(R.id.pvp_mode);
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
