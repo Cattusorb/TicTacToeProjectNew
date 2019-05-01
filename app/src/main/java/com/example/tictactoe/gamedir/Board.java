1a258c2cb8e7d5523f6275793502c1e91db3f514
Roslyn Parker <magicmachineplaymc@gmail.com>
2019-05-01 09:52:14 -0400

    Update Board.java
    not perfect, but a start to how the board class will work

--- app/src/main/java/com/example/tictactoe/gamedir/Board.java
+++ app/src/main/java/com/example/tictactoe/gamedir/Board.java
@@ -1,6 +1,52 @@
 package com.example.tictactoe.gamedir;
 
 public class Board {
-
-    // the board, with spaces available
+    /** List of Integers from 1 - 9 */ 
+    private List<Integer> places;
+    /** Moves that players have made
+        0 means no one has taken the space
+        1 means player one has taken the space
+        2 means player two has taken the space*/ 
+    private int[] playerMoves; 
+    
+    public Board() { 
+        places = new ArrayList()<>; 
+        for(int i = 0; i < 9; i++) { 
+            places.add(i + 1);  
+        }
+    } 
+    
+    public int[] getPlaces() { 
+        return places; 
+    } 
+    
+    public int[] getMoves() { 
+        return playerMoves;
+    } 
+    
+    /** 
+    * Places a move on the board
+    * @param i - place the player will move
+    * @param playerNum - the player who is playing the move
+    */ 
+    public void playMove(int i, int playerNum) { 
+        if (playerNum == 1) { 
+            if (playerMoves[i] == 0) { 
+                 playermoves[i] = 1; 
+                 places.remove(i); 
+            } else { 
+                System.out.print"Someone already moved here, cannot play here"); 
+            }
+        } 
+        if (playerNum == 2) { 
+            if (playerMoves[i] == 0) { 
+                 playermoves[i] = 2; 
+            } else { 
+                System.out.print"Someone already moved here, cannot play here, try another space!"); 
+            }
+        } 
+    }
+    
+    // Put checkWin() in here
+        
 }
